5220f132c28f4ffaebca96a573b3fa828f52ea40
Veronika Pfafenrot <pfafenrot_nika@mail.ru>
2019-08-03 23:52:03 +0600

    Add files via upload

---
+++ Eyelash/gulpfile.js
@@ -0,0 +1,89 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var autoprefixer = require("autoprefixer");
+var minify = require("gulp-minify-css");
+var htmlmin = require("gulp-htmlmin");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var rename = require("gulp-rename");
+var server = require("browser-sync").create();
+var run = require("run-sequence");
+var del = require("del");
+
+
+gulp.task("style", function () {
+	gulp.src("less/style.less")
+	  .pipe(plumber())
+	  .pipe(less())
+	  .pipe(postcss([
+	  	autoprefixer()
+	  ]))
+	  .pipe(gulp.dest("build/css"))
+	  .pipe(minify())
+	  .pipe(rename("style.min.css"))
+	  .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("html", function () {
+	return gulp.src("*.html")
+	  .pipe(posthtml([
+	  	include()
+	  ]))
+	  .pipe(htmlmin({collapseWhitespace: true}))
+	  .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function () {
+	return gulp.src("img/**/*.{png,jpg,svg}")
+	  .pipe(imagemin([
+	  	imagemin.optipng({optimizationLevel: 3}),
+	  	imagemin.jpegtran({progressive: true}),
+	  	imagemin.svgo()
+	  ]))
+	  .pipe(gulp.dest("img"));
+});
+
+gulp.task("webp", function () {
+	return gulp.src("img/**/*.{png,jpg}")
+	  .pipe(webp({quality: 90}))
+	  .pipe(gulp.dest("img"));
+});
+
+gulp.task("serve", function () {
+	server.init({
+		server: "build/"
+	});
+	gulp.watch("less/**/*.less", ["style"]);
+	gulp.watch("*.html", ["html"]);
+});
+
+gulp.task("copy", function () {
+	return gulp.src([
+		"css/**",
+		"fonts/**",
+		"img/**",
+		"js/**",
+		"form/**",
+		".htaccess"
+	  ], {
+		base: "."
+	  })
+	  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+	return del("build");
+});
+
+gulp.task("build", function (done) {
+	run("clean", "copy", "style", "html", done);
+});
+
+
